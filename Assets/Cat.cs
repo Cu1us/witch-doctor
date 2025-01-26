c48b386f9a61a7c2661676fac7a43e71790f1b4b
Måns Fritiofsson <mans@fritiofson.net>
2025-01-26 14:28:08 +0100

    Bubbles and cat

---
+++ Assets/Cat.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    public Renderer headRenderer;
+    public Material unblink;
+    public Material blink;
+
+    void Start()
+    {
+        InvokeRepeating(nameof(Blink), 4, 3f);
+    }
+
+    void Blink()
+    {
+        if (Random.value < 0.4f) return;
+        Invoke(nameof(Unblink), 0.25f);
+        headRenderer.sharedMaterial = blink;
+        // Random chance to play cat sound here?
+    }
+    void Unblink()
+    {
+        headRenderer.sharedMaterial = unblink;
+    }
+}
